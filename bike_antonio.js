dde3bb52ea3380a3116ab16ef13b2c0fdc30d657
JaimeVelasco <jaime@photojv.com>
2016-02-10 20:32:35 -0600

    Fixed issue where nav menu a imation did not work on load, but only after resizing the window

--- bike_antonio.js
+++ bike_antonio.js
@@ -28,8 +28,10 @@
 if (Meteor.isClient) {
 
   Template.main.onCreated(function() {
+    window.requestAnimationFrame(layerInit);
+  });
 
-    layerInit();
+  Template.main.onRendered(function() {
     $(window).on('resize', function(){
       window.requestAnimationFrame(layerInit);
     });
@@ -41,54 +43,51 @@ if (Meteor.isClient) {
   Template.main.events({
 
     "click .cd-nav-trigger": function(){
-      // console.log(this);
+
       if(!$('.cd-nav-trigger').hasClass('close-nav')) {
-           //it means navigation is not visible yet - open it and animate navigation layer
-           $('.cd-nav-trigger').addClass('close-nav');
+         //it means navigation is not visible yet - open it and animate navigation layer
+         $('.cd-nav-trigger').addClass('close-nav');
+
+         $('.cd-overlay-nav').children('span').velocity({
+           translateZ: 0,
+           scaleX: 1,
+           scaleY: 1,
+         }, 500, 'easeInCubic', function(){
+           $('.cd-primary-nav').addClass('fade-in');
+         });
+       } else {
+         //navigation is open - close it and remove navigation layer
+         $('.cd-nav-trigger').removeClass('close-nav');
+
+         $('.cd-overlay-content').children('span').velocity({
+           translateZ: 0,
+           scaleX: 1,
+           scaleY: 1,
+         }, 500, 'easeInCubic', function(){
+           $('.cd-primary-nav').removeClass('fade-in');
 
            $('.cd-overlay-nav').children('span').velocity({
              translateZ: 0,
-             scaleX: 1,
-             scaleY: 1,
-           }, 500, 'easeInCubic', function(){
-             $('.cd-primary-nav').addClass('fade-in');
-           });
-         } else {
-           //navigation is open - close it and remove navigation layer
-           $('.cd-nav-trigger').removeClass('close-nav');
+             scaleX: 0,
+             scaleY: 0,
+           }, 0);
 
-           $('.cd-overlay-content').children('span').velocity({
-             translateZ: 0,
-             scaleX: 1,
-             scaleY: 1,
-           }, 500, 'easeInCubic', function(){
-             $('.cd-primary-nav').removeClass('fade-in');
-
-             $('.cd-overlay-nav').children('span').velocity({
+           $('.cd-overlay-content').addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+             $('.cd-overlay-content').children('span').velocity({
                translateZ: 0,
                scaleX: 0,
                scaleY: 0,
-             }, 0);
-
-             $('.cd-overlay-content').addClass('is-hidden').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
-               $('.cd-overlay-content').children('span').velocity({
-                 translateZ: 0,
-                 scaleX: 0,
-                 scaleY: 0,
-               }, 0, function(){$('.cd-overlay-content').removeClass('is-hidden');});
-             });
-             if($('html').hasClass('no-csstransitions')) {
-               $('.cd-overlay-content').children('span').velocity({
-                 translateZ: 0,
-                 scaleX: 0,
-                 scaleY: 0,
-               }, 0, function(){$('.cd-overlay-content').removeClass('is-hidden');});
-             }
+             }, 0, function(){$('.cd-overlay-content').removeClass('is-hidden');});
            });
-         }
-
-
-
+           if($('html').hasClass('no-csstransitions')) {
+             $('.cd-overlay-content').children('span').velocity({
+               translateZ: 0,
+               scaleX: 0,
+               scaleY: 0,
+             }, 0, function(){$('.cd-overlay-content').removeClass('is-hidden');});
+           }
+         });
+       }
     }
   });
 
